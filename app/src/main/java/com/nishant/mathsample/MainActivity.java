035985a49c1648ca3dd113cbaa4a3fd7923dda69
nishant <nishantpathak.cse@gmail.com>
2017-01-15 19:39:04 +0530

    initial fix

---
+++ app/src/main/java/com/nishant/mathsample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.nishant.mathsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.nishant.math.MathView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.activity_main)
+  LinearLayout linearLayout;
+
+  @BindView(R.id.math_view)
+  MathView mathView;
+
+  @BindView(R.id.input_view)
+  EditText inputView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+    mathView.setText("`sum_(i=1)^n i^3=((n(n+1))/2)^2` $2 = x$ $$ in new line 2+ 5 = 10$$");
+
+    inputView.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+        mathView.setText(inputView.getText().toString());
+      }
+    });
+  }
+
+
+}
